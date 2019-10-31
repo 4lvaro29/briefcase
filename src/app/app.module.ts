27d2c9b933af480b910eda1a50b2211d69051ddb
alvaro <4lvaro.up@gmail.com>
2019-10-30 21:39:44 -0300

    ADD: angular material and card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainBriefcaseComponent } from './components/main-briefcase/main-briefcase.component';
 import { WorksComponent } from './components/works/works.component';
-
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     MainBriefcaseComponent,
     WorksComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule
+    
   ],
+  exports: [
+    MatCardModule
+  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
