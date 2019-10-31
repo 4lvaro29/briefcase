d14f076973c3b6358c388d6cfe8bfa1f86929562
alvaro <4lvaro.up@gmail.com>
2019-10-31 11:29:11 -0300

    ADD: FontAwesome library and styles in briefcase layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,20 @@ import {
   MatMenuModule,
   MatToolbarModule,
   MatIconModule,
-  MatCardModule
+  MatCardModule,
+  MatChipsModule,
+  MatChipList
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee,
+         faEdit,
+         faEye,
+         faClock,
+         faGraduationCap,
+         faBriefcase
+        } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,14 +34,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
-    BrowserAnimationsModule
+    MatChipsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
     
   ],
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule
   ],
   
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    library.add(faCoffee);
+    library.add(faEye);
+    library.add(faClock);
+    library.add(faGraduationCap);
+    library.add(faBriefcase);
+  }
+ }
--- src/app/components/main-briefcase/main-briefcase.component.ts
+++ src/app/components/main-briefcase/main-briefcase.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material';
+
+export interface ChipColor {
+  name: string;
+  color: ThemePalette;
+}
 
 @Component({
   selector: 'app-main-briefcase',
@@ -6,7 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-briefcase.component.css']
 })
 export class MainBriefcaseComponent implements OnInit {
-
+  
+  availableColors: ChipColor[] = [
+    {name: 'none', color: 'primary'},
+    {name: 'Primary', color: 'primary'},
+    {name: 'Accent', color: 'primary'},
+    {name: 'Warn', color: 'primary'}
+  ];
   constructor() { }
 
   ngOnInit() {
